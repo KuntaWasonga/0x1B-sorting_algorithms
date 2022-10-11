fd63daa4c515fbe96c8eb0a3ba39a8cd7d2ce467
CheropS <sh.cherop@gmail.com>
2022-10-11 18:59:45 +0000

    final tuch of deck.h

--- deck.h
+++ deck.h
@@ -46,5 +46,8 @@ typedef struct deck_node_s
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
+int card_value(deck_node_t *node);
+int _strcmp(const char *s1, const char *s2);
+size_t list_len_deck(deck_node_t *list);
 
 #endif /* DECK_H */
